f745f486c230133805b6c272bc3e571f72ac181b
Ebraam Ehab <ibraamehab2005@gmail.com>
2026-07-05 00:14:28 +0300

    refactor: integrate TopHeadline component into HomeScreen; update CategoriesList layout with ViewAllComponent

--- lib/features/home/components/categories_list.dart
+++ lib/features/home/components/categories_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/theme/light_color.dart';
+import 'package:news_app/features/home/components/view_all_component.dart';
 import 'package:news_app/features/home/home_controller.dart';
 import 'package:provider/provider.dart';
 
@@ -12,50 +13,65 @@ class CategoriesList extends StatelessWidget {
       child: Consumer<HomeController>(
         builder:
             (BuildContext context, HomeController controller, Widget? child) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16),
-                child: SizedBox(
-                  height: 35,
-                  child: ListView.separated(
-                    padding: EdgeInsets.only(right: 16),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: categories.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      bool isSelected =
-                          categories[index] == controller.selectedCategory;
-                      return GestureDetector(
-                        onTap: () {
-                          controller.updateSelectedCategory(categories[index]);
-                        },
-                        child: IntrinsicWidth(
-                          child: Column(
-                            children: [
-                              Text(
-                                categories[index][0].toUpperCase() +
-                                    categories[index].substring(1),
-                                style: TextStyle(
-                                  color: Color(0xFF363636),
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                              if (isSelected) ...[
-                                SizedBox(height: 6),
-                                Container(
-                                  height: 2,
-                                  color: LightColors.primaryColor,
-                                ),
-                              ],
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return SizedBox(width: 12);
-                    },
+              return Column(
+                children: [
+                  ViewAllComponent(
+                    title: 'Categories',
+                    titleColor: Color(0xFF141414),
+                    onTap: () {},
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16,
+                      top: 16,
+                      bottom: 16,
+                    ),
+                    child: SizedBox(
+                      height: 35,
+                      child: ListView.separated(
+                        padding: EdgeInsets.only(right: 16),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categories.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          bool isSelected =
+                              categories[index] == controller.selectedCategory;
+                          return GestureDetector(
+                            onTap: () {
+                              controller.updateSelectedCategory(
+                                categories[index],
+                              );
+                            },
+                            child: IntrinsicWidth(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    categories[index][0].toUpperCase() +
+                                        categories[index].substring(1),
+                                    style: TextStyle(
+                                      color: Color(0xFF363636),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  if (isSelected) ...[
+                                    SizedBox(height: 6),
+                                    Container(
+                                      height: 2,
+                                      color: LightColors.primaryColor,
+                                    ),
+                                  ],
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(width: 12);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               );
             },
       ),
---
+++ lib/features/home/components/top_headline.dart
@@ -0,0 +1,132 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:news_app/core/extentions/date_time_extenion.dart';
+import 'package:news_app/features/home/home_controller.dart';
+import 'package:provider/provider.dart';
+
+class TopHeadline extends StatelessWidget {
+  const TopHeadline({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<HomeController>(
+      builder: (BuildContext context, HomeController controller, Widget? child) {
+        return SliverList.builder(
+          itemCount: controller.newsTopHeadLineList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final model = controller.newsTopHeadLineList[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8,
+              ),
+              child: Row(
+                children: [
+                  model.urlToImage != null && model.urlToImage!.isNotEmpty
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            model.urlToImage ?? "",
+                            height: 80,
+                            width: 180,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                height: 70,
+                                width: 120,
+                                color: Colors.grey[300],
+                                child: const Icon(
+                                  Icons.broken_image,
+                                  color: Colors.grey,
+                                ),
+                              );
+                            },
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) return child;
+                              return const SizedBox(
+                                height: 70,
+                                width: 120,
+                                child: Center(
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      : Container(
+                          height: 70,
+                          width: 120,
+                          color: Colors.grey[300],
+                          child: const Icon(
+                            Icons.image_not_supported,
+                            color: Colors.grey,
+                          ),
+                        ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          model.title.toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 2,
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 10,
+                              backgroundImage: NetworkImage(
+                                model.urlToImage ?? "",
+                              ),
+                            ),
+                            SizedBox(width: 6),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    (model.author ?? "").substring(
+                                      0,
+                                      min((model.author ?? "").length, 10),
+                                    ),
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color(0xFF141414),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text(
+                                    model.publishedAt.formatDateTime(),
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color(0xFF141414),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
